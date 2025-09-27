ddbbe4dc97c3d31d56e3475e0df8c1226523ac36
Niko Nevatie <nnevatie@gmail.com>
2025-09-27 15:52:03 +0300

    Add a basic uWS listener to helloworld/server.cpp.

--- examples/helloworld/server.cpp
+++ examples/helloworld/server.cpp
@@ -1,4 +1,17 @@
+#include <App.h>
+
 int main(int argc, char** argv)
 {
+    const auto port = argc > 1 ? std::atoi(argv[1]) : 8000;
+    uWS::App().get("/*", [](auto* res, auto* req)
+    {
+        res->end("Hello World!");
+    })
+    .listen(port, [=](auto* token)
+    {
+        if (token)
+            std::cout << "Listening on port " << port << std::endl;
+    })
+    .run();
     return 0;
 }
